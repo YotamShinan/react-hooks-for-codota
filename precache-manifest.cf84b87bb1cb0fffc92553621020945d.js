3fc1475d098db4a2d82e7a12909fde472fcca750
YotamShinan <yshinan@gmail.com>
2020-06-14 20:32:37 +0300

    commiting

---
+++ precache-manifest.cf84b87bb1cb0fffc92553621020945d.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5cd792dbb1f5391e2d07a174b9038fd",
+    "url": "/react-hooks-for-codota/index.html"
+  },
+  {
+    "revision": "5463614e1f75f4258db5",
+    "url": "/react-hooks-for-codota/static/css/main.cf6cf1f2.chunk.css"
+  },
+  {
+    "revision": "a5b85f41604efb69160c",
+    "url": "/react-hooks-for-codota/static/js/2.396df124.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-hooks-for-codota/static/js/2.396df124.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5463614e1f75f4258db5",
+    "url": "/react-hooks-for-codota/static/js/main.45df23e9.chunk.js"
+  },
+  {
+    "revision": "59924f72c1881bf8c0da",
+    "url": "/react-hooks-for-codota/static/js/runtime-main.f5f3d046.js"
+  },
+  {
+    "revision": "5314dab9b932b65cc40c86ef69aff264",
+    "url": "/react-hooks-for-codota/static/media/Aclonica-Regular.5314dab9.ttf"
+  },
+  {
+    "revision": "ba13edfb3ac4816c9069a1639e896bf0",
+    "url": "/react-hooks-for-codota/static/media/Birly-0WDBv.ba13edfb.otf"
+  },
+  {
+    "revision": "9ccb885c9cf8e503d557f15e0b2cbf24",
+    "url": "/react-hooks-for-codota/static/media/Merriweather-Bold.9ccb885c.ttf"
+  },
+  {
+    "revision": "63e1f06ea6bf31c8f3143c143e7e6c2a",
+    "url": "/react-hooks-for-codota/static/media/Merriweather-BoldItalic.63e1f06e.ttf"
+  },
+  {
+    "revision": "498bf4ee4ac9fab22ad9f814839173b2",
+    "url": "/react-hooks-for-codota/static/media/Merriweather-Italic.498bf4ee.ttf"
+  },
+  {
+    "revision": "c97a9fc29652bb4afcdac68020e5d0f7",
+    "url": "/react-hooks-for-codota/static/media/Merriweather-Regular.c97a9fc2.ttf"
+  },
+  {
+    "revision": "11eabca2251325cfc5589c9c6fb57b46",
+    "url": "/react-hooks-for-codota/static/media/Roboto-Regular.11eabca2.ttf"
+  },
+  {
+    "revision": "0e7e9a9b5c4abaadef7bc8f4e4574084",
+    "url": "/react-hooks-for-codota/static/media/Segoe-UI.0e7e9a9b.ttf"
+  },
+  {
+    "revision": "8ec04ac829a7ea0d0b4d684e40e46a80",
+    "url": "/react-hooks-for-codota/static/media/Teko-Bold.8ec04ac8.ttf"
+  },
+  {
+    "revision": "8e22c2a6e3a3c679787e763a97fa11f7",
+    "url": "/react-hooks-for-codota/static/media/Ubuntu-Medium.8e22c2a6.ttf"
+  }
+]);
